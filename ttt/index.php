2e9719f772ae16a3c5fb8c8976bfc58c84588f4e
Colin Narans <cnarans@gmail.com>
2016-06-24 12:21:40 -0500

    ttt layout

---
+++ ttt/index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Tic-Tac-Toe</title>
+		<link type="text/css" rel="stylesheet" href="style.css"/>
+	</head>
+
+	<body>
+		<div class = "wrapper">
+			<div class = "row">
+				<div class = "cell">
+					X
+				</div>
+				<div class = "cell">
+					Y
+				</div>
+				<div class = "cell">
+					Z
+				</div>
+			</div>
+			<div class = "row">
+				<div class = "cell">
+					X
+				</div>
+				<div class = "cell">
+					Y
+				</div>
+				<div class = "cell">
+					Z
+				</div>
+			</div>
+			<div class = "row">
+				<div class = "cell">
+					X
+				</div>
+				<div class = "cell">
+					Y
+				</div>
+				<div class = "cell">
+					Z
+				</div>
+			</div>
+		</div>
+	</body>
+
+</html>
