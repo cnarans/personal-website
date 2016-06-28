2a11a1338d992ada509e401278eed09ad3c44d06
Colin Narans <cnarans@gmail.com>
2016-06-28 10:53:31 -0500

    working tests

--- rps/test.php
+++ rps/test.php
@@ -1,4 +1,68 @@
 <?php
 	include 'script.php';
-	game("rock", "paper");
+
+	echo "\nTesting Rock/Paper/Scissors\n";
+
+	if(strpos(game("rock", "rock"), "both")){
+		echo "\nRock/Rock Success\n";
+	}
+	else{
+		echo "\nRock/Rock Failure\n";
+	}
+
+	if(strpos(game("rock", "paper"), "loses")){
+		echo "\nRock/Paper Success\n";
+	}
+	else{
+		echo "\nRock/Paper Failure\n";
+	}
+
+	if(strpos(game("rock", "scissors"), "WIN")){
+		echo "\nRock/Scissors Success\n";
+	}
+	else{
+		echo "\nRock/Scissors Failure\n";
+	}
+
+	if(strpos(game("paper", "paper"), "both")){
+		echo "\nPaper/Paper Success\n";
+	}
+	else{
+		echo "\nPaper/Paper Failure\n";
+	}
+
+	if(strpos(game("paper", "scissors"), "loses")){
+		echo "\nPaper/Scissors Success\n";
+	}
+	else{
+		echo "\nPaper/Scissors Failure\n";
+	}
+
+	if(strpos(game("paper", "rock"), "WIN")){
+		echo "\nPaper/Rock Success\n";
+	}
+	else{
+		echo "\nPaper/Rock Failure\n";
+	}
+
+	if(strpos(game("scissors", "scissors"), "both")){
+		echo "\nScissors/Scissors Success\n";
+	}
+	else{
+		echo "\nScissors/Scissors Failure\n";
+	}
+
+	if(strpos(game("scissors", "rock"), "loses")){
+		echo "\nScissors/Rock Success\n";
+	}
+	else{
+		echo "\nScissors/Rock Failure\n";
+	}
+
+	if(strpos(game("scissors", "paper"), "WIN")){
+		echo "\nScissors/Paper Success\n";
+	}
+	else{
+		echo "\nScissors/Paper Failure\n";
+	}
 ?>
