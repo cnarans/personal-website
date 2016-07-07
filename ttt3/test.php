9805376229cb8fb9c6ab5b0122034e983675ab53
Colin Narans <cnarans@gmail.com>
2016-07-07 15:37:57 -0500

    added old test files

---
+++ ttt3/test.php
@@ -0,0 +1,128 @@
+<?php
+	include 'script.php';
+	
+	#
+	# tests the functionality of the turnStatus() function in script.php
+	#
+
+	echo "\nTesting Tic-Tac-Toe functions checkStatus() and checkTurn()\n";
+
+	$x = "991119911";
+	$y = 4;
+	if(turnStatus($x)==$y){
+		echo "\nDraw Success\n";
+	}
+	else{
+		echo "\nDraw Failure\n";
+	}
+
+	$x = "111910909";
+	$y = 2;
+	if(turnStatus($x)==$y){
+		echo "\nX Win Success\n";
+	}
+	else{
+		echo "\nX Win Failure\n";
+	}
+
+	$x = "911990911";
+	$y = 3;
+	if(turnStatus($x)==$y){
+		echo "\nO Win Success\n";
+	}
+	else{
+		echo "\nO Win Failure\n";
+	}
+
+	echo "\nPlaying through a typical game\n";
+
+	$x = "000000000";
+	$y = 1;
+	if(turnStatus($x)==$y){
+		echo "\nBegin with X Success\n";
+	}
+	else{
+		echo "\nBegin with X Failure\n";
+	}
+
+	$x = "000010000";
+	$y = 9;
+	if(turnStatus($x)==$y){
+		echo "\nO Turn Success\n";
+	}
+	else{
+		echo "\nO Turn Failure\n";
+	}
+
+	$x = "900010000";
+	$y = 1;
+	if(turnStatus($x)==$y){
+		echo "\nX Turn Success\n";
+	}
+	else{
+		echo "\nX Turn Failure\n";
+	}
+
+	$x = "901010000";
+	$y = 9;
+	if(turnStatus($x)==$y){
+		echo "\nO Turn Success\n";
+	}
+	else{
+		echo "\nO Turn Failure\n";
+	}
+
+	$x = "901010900";
+	$y = 1;
+	if(turnStatus($x)==$y){
+		echo "\nX Turn Success\n";
+	}
+	else{
+		echo "\nX Turn Failure\n";
+	}
+
+	$x = "901110900";
+	$y = 9;
+	if(turnStatus($x)==$y){
+		echo "\nO Turn Success\n";
+	}
+	else{
+		echo "\nO Turn Failure\n";
+	}
+
+	$x = "901119900";
+	$y = 1;
+	if(turnStatus($x)==$y){
+		echo "\nX Turn Success\n";
+	}
+	else{
+		echo "\nX Turn Failure\n";
+	}
+
+	$x = "911119900";
+	$y = 9;
+	if(turnStatus($x)==$y){
+		echo "\nO Turn Success\n";
+	}
+	else{
+		echo "\nO Turn Failure\n";
+	}
+
+	$x = "911119990";
+	$y = 1;
+	if(turnStatus($x)==$y){
+		echo "\nX Turn Success\n";
+	}
+	else{
+		echo "\nX Turn Failure\n";
+	}
+
+	$x = "911119991";
+	$y = 4;
+	if(turnStatus($x)==$y){
+		echo "\nDraw Success\n";
+	}
+	else{
+		echo "\nDraw Failure\n";
+	}
+?>
---
+++ ttt3/test3.php
@@ -0,0 +1,4 @@
+<?php
+	include 'script.php';
+	echo turnStatus("911119991");
+?>
