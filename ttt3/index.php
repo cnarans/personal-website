c7a8505f36e7c0aa0b5eabbc5efe6e426448fe83
Colin Narans <cnarans@gmail.com>
2016-07-07 13:48:30 -0500

    basics working again

--- ttt3/index.php
+++ ttt3/index.php
@@ -1,8 +1,29 @@
 <?php 
 session_start();
 include 'script.php';
-$pos = "00000000011";
-$turn = "1";
+if($_GET["reset"]){
+	unset($_SESSION["score"]);
+}
+else{
+	$_SESSION["score"] = storeScore($_SESSION["score"], $turn);
+}
+
+if(isset($_GET["state"])){
+	$state = $_GET["state"];
+}
+else{
+	$state = "00000000011";
+}
+
+$turn = turnStatus($state);
+
+if($state[9]==2&&$state[10]==$turn){
+	$state = aiMove($state);
+	$turn = turnStatus($state);
+}
+
+$coin = rand(0,1);
+if($coin==0){$coin=9;}
 ?>
 
 <!DOCTYPE html>
@@ -14,49 +35,104 @@ $turn = "1";
 	</head>
 	<body>
 		<div class = "wrapper">
-			<div class="top">
+			<div class = "top">
 				<div class = "arrow"><a href="../"><img src="White_left_arrow.svg" alt="back arrow"></a></div>
 				<div class = "status">
-					Status
+					<?php
+						if($turn=="1"){
+							echo "X's Turn<br>";
+						}
+						elseif($turn=="9"){
+							echo "O's Turn";
+						}
+						elseif($turn=="2"){
+							echo "X Wins!<br>";
+							echo '<a class="none"href="index.php?state=000000000' . $state[9] . $coin . '">Play Again?</a>';
+						}
+						elseif($turn=="3"){
+							echo "O Wins!<br>";
+							echo '<a class="none"href="index.php?state=000000000' . $state[9] . $coin . '">Play Again?</a>';
+						}
+						elseif($turn=="4"){
+							echo "It's a Draw<br>";
+							echo '<a class="none"href="index.php?state=000000000' . $state[9] . $coin . '">Play Again?</a>';
+						}
+						else{
+							echo "ERROR<br>";
+							echo '<a class="none"href="index.php?state=000000000' . $state[9] . $coin . '">Play Again?</a>';
+						}
+					?>
 				</div>
 			</div>
 			<div class = "row">
 				<div class = "cell">
-					<?php printSquare($pos, 0, $turn) ?>
+					<?php printSquare($state, 0, $turn) ?>
 				</div>
 				<div class = "cell">
-					<?php printSquare($pos, 1, $turn) ?>
+					<?php printSquare($state, 1, $turn) ?>
 				</div>
 				<div class = "cell">
-					<?php printSquare($pos, 2, $turn) ?>
+					<?php printSquare($state, 2, $turn) ?>
 				</div>
 			</div>
 			<div class = "row">
 				<div class = "cell">
-					<?php printSquare($pos, 3, $turn) ?>
+					<?php printSquare($state, 3, $turn) ?>
 				</div>
 				<div class = "cell">
-					<?php printSquare($pos, 4, $turn) ?>
+					<?php printSquare($state, 4, $turn) ?>
 				</div>
 				<div class = "cell">
-					<?php printSquare($pos, 5, $turn) ?>
+					<?php printSquare($state, 5, $turn) ?>
 				</div>
 			</div>
 			<div class = "row">
 				<div class = "cell">
-					<?php printSquare($pos, 6, $turn) ?>
+					<?php printSquare($state, 6, $turn) ?>
 				</div>
 				<div class = "cell">
-					<?php printSquare($pos, 7, $turn) ?>
+					<?php printSquare($state, 7, $turn) ?>
 				</div>
 				<div class = "cell">
-					<?php printSquare($pos, 8, $turn) ?>
+					<?php printSquare($state, 8, $turn) ?>
 				</div>
 			</div>
-			<h2>Switch Players</h2>
+			<h2><?php
+				if($state[9]==2){
+					echo '<a href="index.php">Play against a human instead</a><br>';
+				}
+				else{
+					echo '<a href="index.php?state=0000000002' . $coin . '">Play against the computer instead</a><br>';
+				}
+			?></h2>
 			<div class = "bottom">
 				<h2><a href="index.php?reset=true">(Reset Records)</a></h2>
-				Records Here
+				<?php if($state[9]==1){?>
+				<div class="results">
+					<div class = "results__ident">X</div>
+					<div class = "results__number"><?php echo $_SESSION["xwin"]; ?></div>
+				</div>
+				<div class="results">
+					<div class = "results__ident">O</div>
+					<div class = "results__number"><?php echo $_SESSION["owin"]; ?></div>
+				</div>
+				<div class="results">
+					<div class = "results__ident">Draw</div>
+					<div class = "results__number"><?php echo $_SESSION["hdraw"]; ?></div>
+				</div><?php }
+				else{ ?>
+				<div class="results">
+					<div class = "results__ident">Human</div>
+					<div class = "results__number"><?php echo $_SESSION["hwin"]; ?></div>
+				</div>
+				<div class="results">
+					<div class = "results__ident">Computer</div>
+					<div class = "results__number"><?php echo $_SESSION["cwin"]; ?></div>
+				</div>
+				<div class="results">
+					<div class = "results__ident">Draw</div>
+					<div class = "results__number"><?php echo $_SESSION["cdraw"]; ?></div>
+				</div><?php } ?>
 			</div>
 		</div>
 	</body>
--- ttt3/script.php
+++ ttt3/script.php
@@ -2,16 +2,134 @@
 
 function printSquare($state, $square, $turn){
 	if($state[$square]=="0"){
-			$state[$square] = $turn;
-			echo '<a style = "color:#4AA555" href="index.php?state=' . $state . '">#</a>';
+		$state[$square] = $turn;
+		echo '<a style = "color:transparent" href="index.php?state=' . $state . '">#</a>';
+	}
+	elseif($state[$square]=="1"){
+		echo 'X';
+	}
+	elseif($state[$square]=="9"){
+		echo 'O';
+	}
+	}
+
+function checkStatus($state){
+	$win = array();
+	$win[0] = $state[0] . $state[1] . $state[2];
+	$win[1] = $state[3] . $state[4] . $state[5];
+	$win[2] = $state[6] . $state[7] . $state[8];
+	$win[3] = $state[0] . $state[3] . $state[6];
+	$win[4] = $state[1] . $state[4] . $state[7];
+	$win[5] = $state[2] . $state[5] . $state[8];
+	$win[6] = $state[0] . $state[4] . $state[8];
+	$win[7] = $state[2] . $state[4] . $state[6];
+	
+	foreach($win as $chance){
+		if($chance=="111"){
+			return 2;
 		}
-		elseif($state[$square]=="1"){
-			echo 'X';
+		elseif($chance=="999"){
+			return 3;
 		}
-		else{
-			echo 'O';
-		}
+	}
+	if(strpos($state, "0")===false){
+		return 4;
+	}
+	else{
+		return 5;
+	}
 }
 
+function checkTurn($state){
+	$xCount = 0;
+	$oCount = 0;
+	for($i=0; $i<9; $i++){
+		if($state[$i]=="1"){
+			$xCount++;
+		}
+		elseif($state[$i]=="9"){
+			$oCount++;
+		}
+	}
+	if($xCount>$oCount){
+		return 9;
+	}
+	else{
+		return 1;
+	}	
+}
+
+function turnStatus($state){
+	$result = checkStatus($state);
+	if($result!=5){
+		return $result;
+	}
+	else{
+		return checkTurn($state);
+	}
+}
+
+function aiMove($state){
+	# $xo indicates if the computer is X or O
+	$xo = $state[10];
+
+	# This block set $other to the X or O of the opponent
+	# $win and $lose stores  the values returned by checkStatus to see if the player has won or lost
+	if($xo==1){
+		$win = 2;
+		$lose = 3;
+		$other = 9;
+	}
+	else{
+		$win = 3;
+		$lose = 2;
+		$other = 1;
+	}
+
+	# This block checks for each empty square and loads it into an array of valid moves
+	$moves = [];
+	for($i=0;$i<9;$i++){
+		if($state[$i]==0){
+			array_push($moves, $i);
+		}
+	}
+
+	# First it checks to see if the center square is available
+	if(in_array(4, $moves)){
+		$state[4]=$xo;
+		return $state;
+	}
+
+	# Then it checks to see if any square will cause a victory or loss
+	# If not, it selects a corner square
+	else{
+		foreach($moves as $move){
+			$try = $state;
+			$try[$move]=$xo;
+			if(checkStatus($try)==$win){
+				return $try;
+			}
+			$try[$move]=$other;
+			if(checkStatus($try)==$lose){
+				$try[$move]=$xo;
+				return $try;
+			}
+		}
+		foreach($moves as $move){
+			if($state[$move]==0&&($move==0||$move==2||$move==6||$move==8)){
+				$state[$move]=$xo;
+				return $state;
+			}
+		}
+	}
+
+	# Finally, if all other options are taken, it selects a random square
+	$state[$moves[rand(0,count($moves)-1)]] = $xo;
+	return $state;
+}
+
+function storeScore($scores, $turn){
+	return $scores;
+}
 
 ?>
